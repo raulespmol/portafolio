7c8f9c470510d0dc4294d4efbd9d424abe4c67e1
Raul Espinoza M <raul.espmol@gmail.com>
2024-12-23 20:36:05 -0300

    [move] footer (sections > components)

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import Navbar from "./Navbar";
 import ProjectCard from "./ProjectCard";
 import Button from "./Button";
+import Footer from "./Footer";
 
 export {
   Navbar,
   ProjectCard,
-  Button
+  Button,
+  Footer
 }

bdadcef115a8da69075b8f4f7b35b0eb86905381
Raul Espinoza M <raul.espmol@gmail.com>
2024-12-22 23:07:15 -0300

    [update] assets

---
+++ src/assets/icons/index.js
@@ -0,0 +1,19 @@
+import bootstrapColor from "./bootstrap-color.svg";
+import bootstrapFlat from "./bootstrap-flat.svg";
+import nodeColor from "./node-color.svg";
+import nodeFlat from "./node-flat.svg";
+import postgresColor from "./postgres-color.svg";
+import postgresFlat from "./postgres-flat.svg";
+import reactColor from "./react-color.svg";
+import reactFlat from "./react-flat.svg";
+
+export {
+  bootstrapColor,
+  bootstrapFlat,
+  nodeColor,
+  nodeFlat,
+  postgresColor,
+  postgresFlat,
+  reactColor,
+  reactFlat
+}
--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,7 +1,9 @@
 import avatar from './avatar.jpg'
 import placeholder from './placeholder.jpg'
+import mercadoLibros from './mercadolibros-preview.jpg'
 
 export {
   avatar,
-  placeholder
+  placeholder,
+  mercadoLibros
 }
